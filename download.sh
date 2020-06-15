c27b933afba53463f62abd2182c6f5c0cc2667d1
rasoolianbehnam <behnam.rasoulian@gmail.com>
2020-06-15 15:04:13 -0500

    third commit

--- download.sh
+++ download.sh
@@ -1,1 +1,2 @@
 wget https://auburn.box.com/shared/static/xdss4yden9zfh7zcf5hjroaupfk1haou.zip -O models.zip
+wget https://auburn.box.com/shared/static/54kc6fxkeoufsu4vnaph5lmos096hcp6.zip -O data.zip
